157e82273c68a4a3829619d160a4649ee18e7855
Yaroslav Klimas <yarklim@gmail.com>
2022-08-16 15:59:01 +0300

    new algs

--- 3_selection_sort.js
+++ 3_selection_sort.js
@@ -4,7 +4,7 @@ function selectionSort(array) {
 	for (let i = 0; i < array.length; i++) {
 		let indexMin = i;
 		for (let j = i + 1; j < array.length; j++) {
-			if (array[i] < array[indexMin]) {
+			if (array[j] < array[indexMin]) {
 				indexMin = j;
 			}
 		}
---
+++ 5_quick_sort.js
@@ -0,0 +1,23 @@
+const arr = [0, 3, 4, 2, 7, 1, 9, 6, 5, 2, 8, 4, 7, -1, -5, 23, 6, 35, 48, 32];
+
+function quickSort(array) {
+	if (array.length <= 1) {
+		return array;
+	}
+	let pivotIndex = Math.floor(array.length / 2);
+	let pivot = array[pivotIndex];
+	let less = [];
+	let greater = [];
+	for (let i = 0; i < array.length; i++) {
+		if (i === pivotIndex)
+			continue;
+		if (array[i] < pivot) {
+			less.push(array[i]);
+		} else {
+			greater.push(array[i]);
+		}
+	}
+	return [...quickSort(less), pivot, ...quickSort(greater)];
+}
+
+console.log(quickSort(arr));
