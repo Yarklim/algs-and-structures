f4cb43eaad22cb5ee43de198ae05efbf6f4ab6d1
Yaroslav Klimas <yarklim@gmail.com>
2023-01-09 11:20:55 +0200

    binary search

--- 2_binary_search.js
+++ 2_binary_search.js
@@ -1,39 +1,60 @@
-const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-let count = 0;
+const array = [-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-function binarySearch(array, item) {
-	let start = 0;
-	let end = array.length;
-	let middle;
-	let found = false;
-	let position = -1;
-	while (found === false && start <= end) {
-		middle = Math.floor((start + end) / 2);
-		if (array[middle] === item) {
-			found = true;
-			position = middle;
-			return position;
-		}
-		if (item < array[middle]) {
-			end = middle - 1;
-		} else {
-			start = middle + 1;
-		}
-	}
-	return position;
-}
+let search = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+  let mid;
 
-function recursiveBinarySearch(array, item, start, end) {
-	let middle = Math.floor((start + end) / 2);
-	if (item === array[middle]) {
-		return middle;
-	}
-	if (item < array[middle]) {
-		return recursiveBinarySearch(array, item, start, middle - 1);
-	} else {
-		return recursiveBinarySearch(array, item, middle + 1, end);
-	}
-}
+  while (left <= right) {
+    mid = Math.round((right - left) / 2) + left;
 
-console.log(binarySearch(array, 8));
-console.log(recursiveBinarySearch(array, 12, 0, array.length));
+    if (target === nums[mid]) {
+      return mid;
+    } else if (target < nums[mid]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return -1;
+};
+
+console.log(search(array, 8));
+
+// let count = 0;
+// function binarySearch(array, item) {
+// 	let start = 0;
+// 	let end = array.length;
+// 	let middle;
+// 	let found = false;
+// 	let position = -1;
+// 	while (found === false && start <= end) {
+// 		middle = Math.floor((start + end) / 2);
+// 		if (array[middle] === item) {
+// 			found = true;
+// 			position = middle;
+// 			return position;
+// 		}
+// 		if (item < array[middle]) {
+// 			end = middle - 1;
+// 		} else {
+// 			start = middle + 1;
+// 		}
+// 	}
+// 	return position;
+// }
+
+// function recursiveBinarySearch(array, item, start, end) {
+// 	let middle = Math.floor((start + end) / 2);
+// 	if (item === array[middle]) {
+// 		return middle;
+// 	}
+// 	if (item < array[middle]) {
+// 		return recursiveBinarySearch(array, item, start, middle - 1);
+// 	} else {
+// 		return recursiveBinarySearch(array, item, middle + 1, end);
+// 	}
+// }
+
+// console.log(binarySearch(array, 8));
+// console.log(recursiveBinarySearch(array, 12, 0, array.length));
