fc47984ec3ed3a6e7ec2a305ca8c43c399b22102
Yaroslav Klimas <yarklim@gmail.com>
2026-09-25 16:32:48 +0300

    fix: 3_quick_sort.js

--- ALGORITHMS/sort/3_quick_sort.js
+++ ALGORITHMS/sort/3_quick_sort.js
@@ -1,5 +1,13 @@
 // =========== O(n log n), память O(log n) ============
 
+/**
+ * ========= Разделяй и властвуй ==========
+ * Состоит из 3 этапов
+ * 1. Разделяй (Divide)
+ * 2. Властвуй (Conquer)
+ * 3. Объединяй (Combine)
+ */
+
 const arr = [0, 3, 4, 2, 7, 1, 9, 6, 5, 2, 8, 4, 7, -1, -5, 23, 6, 35, 48, 32];
 
 function quickSort(array) {
@@ -8,17 +16,17 @@ function quickSort(array) {
   }
   let pivotIndex = Math.floor(array.length / 2);
   let pivot = array[pivotIndex];
-  let less = [];
-  let greater = [];
+  let left = [];
+  let right = [];
   for (let i = 0; i < array.length; i++) {
     if (i === pivotIndex) continue;
     if (array[i] < pivot) {
-      less.push(array[i]);
+      left.push(array[i]);
     } else {
-      greater.push(array[i]);
+      right.push(array[i]);
     }
   }
-  return [...quickSort(less), pivot, ...quickSort(greater)];
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
 
 console.log(quickSort(arr));
